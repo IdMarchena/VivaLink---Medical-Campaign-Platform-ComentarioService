30e46c5e4de4e3c516a187ff5bfc4dc3a2cbd987
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-15 17:43:27 -0500

    feat(modelo): agregar Campana
    - Modelo de Campaña para asociar comentarios
    - Incluye atributos clave y métodos de acceso

---
+++ src/java/modelo/Campana.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Campana {
+    private int id;
+    private String nombre;
+    private Usuario usuario;
+
+    public Campana(int id, String nombre, Usuario usuario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Campa\u00f1a{" + "id=" + getId() + ", nombre=" + getNombre() + ", usuario=" + getUsuario() + '}';
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
