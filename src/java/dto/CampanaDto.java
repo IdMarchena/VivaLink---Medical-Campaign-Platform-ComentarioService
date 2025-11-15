5400ff9f5a823b4d420626f2d932ae4f8deafff8
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-15 17:41:26 -0500

    feat(dto): agregar CampanaDto
    - Estructura del DTO para campañas asociadas a comentarios
    - Facilita transferencia de datos entre capas
    - Completa el conjunto de DTOs del servicio

---
+++ src/java/dto/CampanaDto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author Usuario
+ */
+public record CampanaDto(int id,
+                         String nombre,
+                         UsuarioDto usuarioDto) {
+
+}
