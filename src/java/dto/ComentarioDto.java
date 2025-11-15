422a841e29a8189c6c929971db386117d88afd7d
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-15 17:42:18 -0500

    feat(dto): actualizar ComentarioDto
    - Ajustes en campos y métodos
    - Mejor compatibilidad con la capa de servicio

--- src/java/dto/ComentarioDto.java
+++ src/java/dto/ComentarioDto.java
@@ -16,6 +16,6 @@ public record ComentarioDto(int id,
                             LocalDate fechaCreacion,
                             LocalDate fechaActualizacion,
                             String estado, 
-                            CampañaDto campaña) {
+                            CampanaDto campaña) {
 
 }
